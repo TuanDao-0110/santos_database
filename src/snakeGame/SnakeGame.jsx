40e84a13174fdf60eb26103eae02c5758bf4d4aa
tuan <rickytuan1991@gmail.com>
2022-11-29 10:17:50 +0200

    add snake

--- src/snakeGame/SnakeGame.jsx
+++ src/snakeGame/SnakeGame.jsx
@@ -1,29 +1,43 @@
+/* eslint-disable no-fallthrough */
 import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 export default function SnakeGame() {
   const canvasRef = useRef();
   const [snake, setSnake] = useState([
-    [0, 10],
-    [0, 20],
-    [0, 30],
-    [0, 40],
-    [10, 40],
+    [50, 150],
+    [50, 160],
+    [50, 170],
+    //[y,x]
   ]);
+  const direction = {};
+  const move = useRef();
   useEffect(() => {
-    console.log("snake move");
+    let context;
+    context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    // context.setTransform(40, 0, 0, 40, 0, 0);
     snake.map((item, index) => {
-      index = canvasRef.current.getContext("2d");
-      index.fillStyle = "green";
-      index.fillRect(item[0], item[1], 10, 10);
+      context.fillStyle = "green";
+      context.fillRect(item[0], item[1], 10, 10);
     });
-    // const context2 = canvasRef.current.getContext("2d");
-    // context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
     const apple = canvasRef.current.getContext("2d");
     apple.fillStyle = "blue";
     apple.fillRect(70, 100, 10, 10);
   }, [snake]);
+  const findOrderDifferent = (arr) => {
+    let orderDifferent = 0;
+    for (let i = 0; i < arr.length-1; i++) {
+      if (arr[i][0] !== arr[i + 1][0]) {
+        orderDifferent = 0;
+      } else {
+        orderDifferent = 1;
+      }
+    }
 
+    return orderDifferent;
+  };
   return (
     <div className="flex justify-center">
       <canvas id="myCanvas" width={400} height={400} ref={canvasRef} style={{ border: "1px solid #000000" }} />
@@ -31,31 +45,28 @@ export default function SnakeGame() {
         onClick={() => {
           setSnake(() => {
             let temp = [...snake];
-            temp[0] += 10;
+            let order = findOrderDifferent(temp);
+            move.current = "up";
+
+            temp.map((item) => {
+              switch (move.current) {
+                case "up":
+                  item[order] -= 10;
+                  break;
+                default:
+                  item[order] += 10;
+              }
+            });
+
             return temp;
           });
-          console.log(snake);
         }}
       >
         move forward
       </button>
-
-      <button
-        onClick={() => {
-          setSnake(() => {
-            let temp = [...snake];
-            temp[0] += 10;
-            return temp;
-          });
-          console.log(snake);
-        }}
-      >
-        move down
-      </button>
-
-      {snake.map((item) => {
-        return <p>{item}</p>;
-      })}
+      <div>
+        <button onClick={() => {}}>turn left</button>
+      </div>
     </div>
   );
 }
