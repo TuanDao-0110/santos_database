a80da4230b98c6a6d37c737917cbf2a3f3c721a5
tuan <rickytuan1991@gmail.com>
2022-11-30 08:16:51 +0200

    add snake

--- src/snakeGame/SnakeGame.jsx
+++ src/snakeGame/SnakeGame.jsx
@@ -9,39 +9,93 @@ export default function SnakeGame() {
     [80, 70],
     [80, 60],
     [80, 50],
+    [80, 40],
+    [80, 30],
+
     //[x,y]
   ]);
-  const move = useRef();
+  const [score, setScore] = useState(0);
   const [direction, setDirection] = useState("straight");
-  const orderDifferent = useRef();
+  const [start, setStart] = useState(false);
+  const [apple, setApple] = useState([300, 300]);
+  const handleKeyDown = (event) => {
+    if (snake[0][0] === snake[1][0]) {
+      if (snake[0][1] > snake[1][1]) {
+        if (event.key === "ArrowDown") {
+          setDirection("right");
+        } else if (event.key === "ArrowUp") {
+          setDirection("left");
+        }
+      } else if (snake[0][1] < snake[1][1]) {
+        if (event.key === "ArrowDown") {
+          setDirection("left");
+        } else if (event.key === "ArrowUp") {
+          setDirection("right");
+        }
+      }
+    } else if (snake[0][0] !== snake[1][0]) {
+      if (snake[0][0] > snake[1][0]) {
+        if (event.key === "ArrowRight") {
+          setDirection("left");
+        } else if (event.key === "ArrowLeft") {
+          setDirection("right");
+        }
+      } else if (snake[0][0] < snake[1][0]) {
+        if (event.key === "ArrowRight") {
+          setDirection("right");
+        } else if (event.key === "ArrowLeft") {
+          setDirection("left");
+        }
+      }
+    }
+  };
+  const ref = useRef(null);
+  const setupFruit = () => {
+    setApple(() => {
+      return [Math.round(Math.random() * 40) * 10, Math.round(Math.random() * 40) * 10];
+    });
+  };
+  const setUpPoint = () => {
+    snake.map((item) => {
+      if (item[0] === apple[0] && item[1] === apple[1]) {
+        console.log('go here')
+        setupFruit();
+        setSnake(() => {
+          let temp = [...snake];
+          temp.push(apple);
+          return temp;
+        });
+        return setScore(() => {
+          let temp = score;
+          temp += 1;
+          return temp;
+        });
+      }
+    });
+  };
   useEffect(() => {
+    setDirection("straight");
     let context;
     context = canvasRef.current.getContext("2d");
     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
     // context.setTransform(40, 0, 0, 40, 0, 0);
     snake.map((item, index) => {
-      context.fillStyle = "green";
+      context.fillStyle = `${index === 0 ? "blue" : "green"}`;
       context.fillRect(item[1], item[0], 10, 10);
     });
+    setUpPoint();
 
-    const apple = canvasRef.current.getContext("2d");
-    apple.fillStyle = "blue";
-    apple.fillRect(70, 100, 10, 10);
-  }, [snake]);
-  const findOrderDifferent = (arr) => {
-    let orderDifferent = 0;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i][0] === arr[i + 1][0]) {
-        orderDifferent = 1;
-      } else {
-        orderDifferent = 0;
-      }
-    }
-    return orderDifferent;
-  };
+    const fruit = canvasRef.current.getContext("2d");
+    fruit.fillStyle = "red";
+    fruit.fillRect(apple[0], apple[1], 10, 10);
+    start &&
+      setTimeout(() => {
+        goStraight();
+      }, 50);
+  }, [snake, apple, score, start]);
+  //   useEffect(() => {}, [snake, start, score]);
 
   const goStraight = () => {
-    orderDifferent.current = findOrderDifferent(snake);
     setSnake(() => {
       let temp = [...snake];
       let firstTemp = [...temp[0]];
@@ -63,17 +117,34 @@ export default function SnakeGame() {
           }
           break;
         case "right":
-          if (temp[0][1] !== temp[1][1]) {
-            firstTemp[0] += 10;
-          } else {
-            firstTemp[1] -= 10;
+          if (temp[0][0] === temp[1][0]) {
+            if (temp[0][1] > temp[1][1]) {
+              firstTemp[0] += 10;
+            } else if (temp[0][1] < temp[1][1]) {
+              firstTemp[0] -= 10;
+            }
+          } else if (temp[0][0] !== temp[1][0]) {
+            if (temp[0][0] > temp[1][0]) {
+              firstTemp[1] -= 10;
+            } else if (temp[0][0] < temp[1][0]) {
+              firstTemp[1] += 10;
+            }
           }
           break;
-        case "left":
-          orderDifferent.current ? (firstTemp[0] -= 10) : (firstTemp[0] -= 10);
-          break;
         default:
-          //   firstTemp[orderDifferent.current === 1 ? 0 : 1] -= 10;
+          if (temp[0][0] === temp[1][0]) {
+            if (temp[0][1] > temp[1][1]) {
+              firstTemp[0] -= 10;
+            } else if (temp[0][1] < temp[1][1]) {
+              firstTemp[0] += 10;
+            }
+          } else if (temp[0][0] !== temp[1][0]) {
+            if (temp[0][0] > temp[1][0]) {
+              firstTemp[1] += 10;
+            } else if (temp[0][0] < temp[1][0]) {
+              firstTemp[1] -= 10;
+            }
+          }
           break;
       }
       temp.unshift(firstTemp);
@@ -82,46 +153,17 @@ export default function SnakeGame() {
   };
 
   return (
-    <div className="flex justify-center">
-      <canvas id="myCanvas" width={400} height={400} ref={canvasRef} style={{ border: "1px solid #000000" }} />
-
+    <div className="flex flex-wrap " ref={ref} tabIndex={-1} onKeyDown={handleKeyDown}>
+      <h1 className="w-full">Your score : {score}</h1>
+      <canvas id="myCanvas" width={400} height={400} ref={canvasRef} style={{ border: "1px solid #000000" }} onKeyDown={handleKeyDown} />
       <div>
-        <button
-          onClick={() => {
-            goStraight();
-          }}
-        >
-          turn go forward down {`->`}
-        </button>
-
         <div className="flex flex-col">
           <button
             onClick={() => {
-              setDirection("straight");
+              setStart(true);
             }}
           >
-            set up straight
-          </button>
-          <button
-            onClick={() => {
-              setDirection("right");
-            }}
-          >
-            set up right
-          </button>
-          <button
-            onClick={() => {
-              setDirection("left");
-            }}
-          >
-            set up left
-          </button>
-          <button
-            onClick={() => {
-              setDirection("back");
-            }}
-          >
-            set up back
+            start
           </button>
         </div>
       </div>
